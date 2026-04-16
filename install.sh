434e8de4f5736485a91be1012b4e429e973e573f
Ragdata <ragdata@users.noreply.github.com>
2026-04-16 12:47:24 +1000

    bug: fix vendor file inclusion

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ set -e
 # LOCAL VARIABLES
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-. vendor/progressbar
+# . vendor/progressbar
 ####################################################################
 # VARIABLES
 ####################################################################
@@ -78,110 +78,7 @@ error::exit() {
 	exit 1
 }
 
-update_server() {
-	TotalSteps=$1
-	StepsDone=${2:-0}
-
-	bar::start
-
-	bar::status_changed $((${StepsDone})) "$TotalSteps"
-
-	print::head "Updating System ..."
-	apt update -qq
-	bar::status_changed $((${StepsDone}+1)) "$TotalSteps"
-
-	print::head "Upgrading System ..."
-	apt full-upgrade -y -qq
-	bar::status_changed $((${StepsDone}+1)) "$TotalSteps"
-
-	print::head "Installing Build Tools ..."
-	for tool in "${tools[@]}"; do
-		if ! apt install -y "$tool" &> /dev/null; then
-			print::warn "Failed to install '$tool'"
-		else
-			print::success "Successfully installed '$tool'"
-		fi
-		StepsDone=$((${StepsDone:-2}+1))
-		bar::status_changed "${StepsDone}" "$TotalSteps"
-	done
-
-	print::head "Cleaning up ..."
-	apt autoremove -y -qq && apt clean -qq
-	bar::status_changed $((${StepsDone}+1)) "$TotalSteps"
-
-	bar::stop
-}
-
 install_files() {
-	print::head "Creating Installation Directories ..."
-	mkdir -p "$HOME"/.backup "$HOME"/.labware
-	cd "$HOME"/.labware
-	mkdir -p lib/alliases lib/completions lib/functions log reg
-	cd --
-
-	print::head "Installing Alias Files ..."
-	for file in "$SCRIPT_DIR"/sys/lib/aliases/*; do
-		if install -m 644 "$SCRIPT_DIR"/sys/lib/aliases/"$file" "$HOME"/.labware/lib/aliases/"$file"; then
-			print::default "  - $file"
-		else
-			print::warn "Failed to install '$file'"
-		fi
-	done
-
-	print::head "Installing Completion Files ..."
-	for file in "$SCRIPT_DIR"/sys/lib/completions/*; do
-		if install -m 644 "$SCRIPT_DIR"/sys/lib/completions/"$file" "$HOME"/.labware/lib/completions/"$file"; then
-			print::default "  - $file"
-		else
-			print::warn "Failed to install '$file'"
-		fi
-	done
-
-	print::head "Installing Function Files ..."
-	for file in "$SCRIPT_DIR"/sys/lib/functions/*; do
-		if install -m 644 "$SCRIPT_DIR"/sys/lib/functions/"$file" "$HOME"/.labware/lib/functions/"$file"; then
-			print::default "  - $file"
-		else
-			print::warn "Failed to install '$file'"
-		fi
-	done
-
-	print::head "Installing DOT Files ..."
-	mkdir -p "$HOME"/.bashrc.d/prompts
-	print::default "  - .bashrc.d"
-	for file in "$SCRIPT_DIR"/sys/dots/.bashrc.d/*; do
-		if [ ! -d "$file" ]; then
-			if install -m 644 "$SCRIPT_DIR"/sys/dots/.bashrc.d/"$file"; then
-				print::default "    - $file"
-			else
-				print::warn "Failed to install '$file'"
-			fi
-		fi
-	done
-	print::default "    - prompts"
-	for file in "$SCRIPT_DIR"/sys/dots/.bashrc.d/prompts/*; do
-		if install -m 644 "$SCRIPT_DIR"/sys/dots/.bashrc.d/prompts/"$file" "$HOME"/.labware/.bashrc.d/prompts/"$file"; then
-			print::default "      - $file"
-		else
-			print::warn "Failed to install '$file'"
-		fi
-	done
-	if ! install -m 644 "$HOME"/.bashrc "$HOME"/.backup/.bashrc.OLD; then
-		print::warn "Failed to backup '$file'"
-	fi
-	if ! install -m 644 "$HOME"/.profile "$HOME"/.backup/.profile; then
-		print::warn "Failed to backup '$file'"
-	fi
-	if install -m 644 "$SCRIPT_DIR"/sys/dots/.bashrc "$HOME"/.bashrc; then
-		print::default "  - .bashrc"
-	else
-		error::exit "Failed to install '.bashrc'"
-	fi
-	if install -m 644 "$SCRIPT_DIR"/sys/dots/.profile "$HOME"/.profile; then
-		print::default "  - .profile"
-	else
-		error::exit "Failed to install '.profile'"
-	fi
 }
 ####################################################################
 # PROCESS
@@ -198,10 +95,91 @@ fi
 
 clear
 
-#bar::start
-#
-#bar::status_changed $((StepsDone)) $((TotalSteps))
+print::head "Updating System ..."
+apt update -qq
 
-print::success "TEST"
+print::head "Upgrading System ..."
+apt full-upgrade -y -qq
 
-#bar::stop
+print::head "Installing Build Tools ...."
+for tool in "${tools[@]}"; do
+	if ! apt install -y "$tool" &> /dev/null; then
+		print::warn "Failed to install '$tool'"
+	else
+		print::success "Successfully installed '$tool'"
+	fi
+done
+
+print::head "Cleaning Up ..."
+apt autoremove -y -qq && apt clean -qq
+
+print::head "Creating Installation Directories ..."
+mkdir -p "$HOME"/.backup "$HOME"/.labware
+cd "$HOME"/.labware
+mkdir -p lib/alliases lib/completions lib/functions log reg
+cd --
+
+print::head "Installing Alias Files ..."
+for file in "$SCRIPT_DIR"/sys/lib/aliases/*; do
+	if install -m 644 "$SCRIPT_DIR"/sys/lib/aliases/"$file" "$HOME"/.labware/lib/aliases/"$file"; then
+		print::default "  - $file"
+	else
+		print::warn "Failed to install '$file'"
+	fi
+done
+
+print::head "Installing Completion Files ..."
+for file in "$SCRIPT_DIR"/sys/lib/completions/*; do
+	if install -m 644 "$SCRIPT_DIR"/sys/lib/completions/"$file" "$HOME"/.labware/lib/completions/"$file"; then
+		print::default "  - $file"
+	else
+		print::warn "Failed to install '$file'"
+	fi
+done
+
+print::head "Installing Function Files ..."
+for file in "$SCRIPT_DIR"/sys/lib/functions/*; do
+	if install -m 644 "$SCRIPT_DIR"/sys/lib/functions/"$file" "$HOME"/.labware/lib/functions/"$file"; then
+		print::default "  - $file"
+	else
+		print::warn "Failed to install '$file'"
+	fi
+done
+
+print::head "Installing DOT Files ..."
+mkdir -p "$HOME"/.bashrc.d/prompts
+print::default "  - .bashrc.d"
+for file in "$SCRIPT_DIR"/sys/dots/.bashrc.d/*; do
+	if [ ! -d "$file" ]; then
+		if install -m 644 "$SCRIPT_DIR"/sys/dots/.bashrc.d/"$file"; then
+			print::default "    - $file"
+		else
+			print::warn "Failed to install '$file'"
+		fi
+	fi
+done
+print::default "    - prompts"
+for file in "$SCRIPT_DIR"/sys/dots/.bashrc.d/prompts/*; do
+	if install -m 644 "$SCRIPT_DIR"/sys/dots/.bashrc.d/prompts/"$file" "$HOME"/.labware/.bashrc.d/prompts/"$file"; then
+		print::default "      - $file"
+	else
+		print::warn "Failed to install '$file'"
+	fi
+done
+if ! install -m 644 "$HOME"/.bashrc "$HOME"/.backup/.bashrc.OLD; then
+	print::warn "Failed to backup '$file'"
+fi
+if ! install -m 644 "$HOME"/.profile "$HOME"/.backup/.profile; then
+	print::warn "Failed to backup '$file'"
+fi
+if install -m 644 "$SCRIPT_DIR"/sys/dots/.bashrc "$HOME"/.bashrc; then
+	print::default "  - .bashrc"
+else
+	error::exit "Failed to install '.bashrc'"
+fi
+if install -m 644 "$SCRIPT_DIR"/sys/dots/.profile "$HOME"/.profile; then
+	print::default "  - .profile"
+else
+	error::exit "Failed to install '.profile'"
+fi
+
