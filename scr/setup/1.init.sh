6b59c884a1fa3f30c0ea1f3b0fcd22f5fa57b861
Ragdata <ragdata@users.noreply.github.com>
2026-05-13 14:08:17 +1000

    feat: update scripts

--- scr/setup/1.init.sh
+++ scr/setup/1.init.sh
@@ -52,4 +52,4 @@ echo -e "${YELLOW}Installing UV ...${NC}"
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 echo -e "${YELLOW}Installing Python Libraries ...${NC}"
-pip install -r requirements.txt --user
+pip install -r requirements.txt --user --break-system-packages
