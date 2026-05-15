d9a7b00395e28a3c4847eaaeb3887b93c57e4041
Ragdata <ragdata@users.noreply.github.com>
2026-05-15 23:49:19 +1000

    refactor: refactor script

--- scr/setup/modules/config.py
+++ scr/setup/modules/config.py
@@ -18,7 +18,7 @@ from configparser import ConfigParser
 from pathlib import Path
 
 BASEDIR = Path(__file__).resolve().parent.parent.parent.parent
-DEFAULT = BASEDIR / "scr" / "setup" / ".default.cfg"
+DEFAULT = BASEDIR / "scr" / "setup" / "config" / ".default.cfg"
 
 if not DEFAULT.exists():
     raise FileNotFoundError(f"Config file not found: '{DEFAULT}'")
