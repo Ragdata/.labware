77d48f7520a0abd0817ad69cce706a0d00339354
Ragdata <ragdata@users.noreply.github.com>
2026-05-27 04:53:54 +1000

    refactor: split out filesystem functions

---
+++ scr/setup/mod/filesys.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+"""
+====================================================================
+Package: labware
+====================================================================
+Author:			Ragdata
+Date:			19/04/2026
+License:		MIT License
+Repository:		https://github.com/Ragdata/.labware
+Copyright:		Copyright © 2026 Redeyed Technologies
+====================================================================
+"""
+import shutil, sys, grp
+
+sys.path.append('.')
+
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
+
+from utils import *
+
+
+#-------------------------------------------------------------------
+# MODULE VARIABLES
+#-------------------------------------------------------------------
+BASEDIR = Path(config.get("paths", "base"))
+TEMPLATES = str(BASEDIR / "scr/setup/etc")
+loader = Environment(loader=FileSystemLoader(TEMPLATES))
+#-------------------------------------------------------------------
+# MODULE FUNCTIONS
+#-------------------------------------------------------------------
+def backup(filepath: Path, backupdir: Path = Path.home() / ".backup") -> bool:
+    """Backup a file to the specified directory"""
+    try:
+        if not filepath.exists():
+            raise FileNotFoundError(f"{filepath} does not exist")
+        if not backupdir.exists():
+            backupdir.mkdir(parents=True, exist_ok=True, mode=0o755)
+        suffix = '.'.join(filepath.suffixes)
+        now = datetime.now()
+        backupfile = backupdir / f"{filepath.name}.{suffix}.{now.strftime('%Y%m%d-%H%M.%S')}"
+        if shutil.copy2(filepath, backupfile):
+            return True
+    except Exception as e:
+        reason = str(e)
+        outlog.logError(f"Failed to get list: {reason}")
+        raise RuntimeError(f"Failed to backup file {filepath}: {e}")
+    return False
+
+def chmod(tgt: Path, mode: int = 0o644) -> None:
+    """Smart / Recursive chmod"""
+    if tgt.exists():
+        os.chmod(tgt, mode)
+        if tgt.is_dir():
+            for root, dirs, files in os.walk(tgt):
+                for d in dirs:
+                    os.chmod(os.path.join(root, d), 0o755)
+                for f in files:
+                    os.chmod(os.path.join(root, f), mode)
+
+def chown(tgt: Path, user: str, group: str) -> None:
+    """Smart / Recursive chown"""
+    if tgt.exists():
+        uid = pwd.getpwnam(user).pw_uid
+        gid = grp.getgrnam(group).gr_gid
+        os.chown(tgt, uid, gid)
+        if tgt.is_dir():
+            for root, dirs, files in os.walk(tgt):
+                for name in dirs + files:
+                    os.chown(os.path.join(root, name), uid, gid)
+
+def copyFiles(src: Path, dst: Path, bkp: bool = False, bkpdir: Path = Path.home() / ".backup", mode: int = 0o644, user: str = "", group: str = "") -> bool:
+    try:
+        if not user:
+            user = pwd.getpwuid(os.geteuid()).pw_name
+        if not group:
+            group = user
+        if not userExists(user):
+            raise RuntimeError(f"User '{user}' does not exist")
+        if not src.exists():
+            raise FileNotFoundError(f"{src} does not exist")
+        if not dst.exists():
+            dst.mkdir(parents=True, mode=0o755)
+        if src.is_file():
+            if dst.is_file() and bkp:
+                backup(src, bkpdir)
+            shutil.copy(src, dst)
+            chown(dst, user, group)
+            chmod(dst, mode)
+            printSuccess(f"Copied {src.name}")
+            logger.debug(f"Copied {src.name}")
+            return True
+        elif src.is_dir() and dst.is_dir():
+            for item in os.scandir(src):
+                dest = dst / item.name
+                if item.is_file():
+                    if dest.is_file() and bkp:
+                        backup(Path(item), bkpdir)
+                    if shutil.copy(item, dest):
+                        chown(dest, user, group)
+                        chmod(dest, mode)
+                        printSuccess(f"Copied '{item.name}'")
+                        logger.debug(f"Copied '{item.name}'")
+                    else:
+                        printWarning(f"Copy Failed '{item.name}'")
+                        logger.debug(f"Copy Failed '{item.name}'")
+                elif item.is_dir():
+                    if shutil.copytree(item, dest, dirs_exist_ok=True):
+                        chown(dest, user, group)
+                        chmod(dest, mode)
+                        printSuccess(f"Copied Tree '{item.name}'")
+                        logger.debug(f"Copied Tree '{item.name}'")
+                    else:
+                        printWarning(f"Copy Tree Failed '{item.name}'")
+                        logger.debug(f"Copy Tree Failed '{item.name}'")
+                else:
+                    raise TypeError(f"Invalid type copying {src} -> {dst}")
+            return True
+        else:
+            raise TypeError(f"Invalid type copying {src} -> {dst}")
+    except Exception as e:
+        reason = str(e)
+        outlog.logError(f"Failed to copy files: {reason}")
+        raise e
+
+def findFileString(filepath: Path, string: str) -> bool:
+    try:
+        if not filepath.exists():
+            raise FileNotFoundError(f"File not found '{filepath}'")
+        with open(str(filepath), 'r') as f:
+            for l in f:
+                if string in l:
+                    return True
+        return False
+    except Exception as e:
+        reason = str(e)
+        outlog.logError(f"{reason}")
+        raise e
+
+def getList(filepath: Path) -> list:
+    try:
+        if not filepath.exists():
+            raise FileNotFoundError(f"{filepath} does not exist")
+        with open(str(filepath), 'r') as f:
+            lines = [l.strip() for l in f]
+            return lines
+    except Exception as e:
+        reason = str(e)
+        outlog.logError(f"Failed to get list: {reason}")
+        raise e
+
+def writeFile(dst: Path, data: str, mode: int = 0o644, user: str = "", group: str = "") -> bool:
+    try:
+        if not user:
+            user = pwd.getpwuid(os.geteuid()).pw_name
+        if not group:
+            group = user
+        if not userExists(user):
+            raise RuntimeError(f"User {user} does not exist")
+        if not dst.parent.exists():
+            dst.parent.mkdir(parents=True, mode=0o755)
+        if dst.exists():
+            os.remove(dst)
+        with open(str(dst), 'w') as f:
+            f.write(data)
+        chown(dst, user, group)
+        chmod(dst, mode)
+        printSuccess(f"Wrote File: {dst}")
+        logger.debug(f"Wrote File: {dst}")
+        return True
+    except Exception as e:
+        reason = str(e)
+        outlog.logError(f"File write failed: {reason}")
+        raise e
+
+def writeTemplate(tmpl: Path, dest: Path, data: dict, mode: int = 0o644, user: str = "", group: str = "") -> bool:
+    try:
+        if not user:
+            user = pwd.getpwuid(os.geteuid()).pw_name
+        if not group:
+            group = user
+        if not userExists(user):
+            raise RuntimeError(f"User {user} does not exist")
+        if not tmpl.exists():
+            raise FileNotFoundError(f"{tmpl} does not exist")
+        if not dest.parent.exists():
+            dest.parent.mkdir(parents=True, mode=0o755)
+        if dest.exists():
+            os.remove(str(dest))
+        template = loader.get_template(str(tmpl))
+        with open(dest, 'w') as f:
+            print(template.render(data), file=f)
+        chown(dest, user, group)
+        chmod(dest, mode)
+        printSuccess(f"Wrote template file to '{dest}'")
+        logger.debug(f"Wrote template file to '{dest}'")
+        return True
+    except Exception as e:
+        reason = str(e)
+        outlog.logError(f"Template write failed: {reason}")
+        raise e
+
