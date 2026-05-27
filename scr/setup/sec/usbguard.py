0bbaf7a155e0d026ace9f63a01173b38a37a0b40
Ragdata <ragdata@users.noreply.github.com>
2026-05-28 07:37:20 +1000

    feat: add usbguard

---
+++ scr/setup/sec/usbguard.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+====================================================================
+Package: labware
+====================================================================
+Author:			Ragdata
+Date:			12/05/2026
+License:		MIT License
+Repository:		https://github.com/Ragdata/.labware
+Copyright:		Copyright © 2026 Redeyed Technologies
+====================================================================
+"""
+import sys
+
+sys.path.append("../mod")
+
+from mod.filesys import *
+
+#-------------------------------------------------------------------
+# VARIABLES
+#-------------------------------------------------------------------
+BASEDIR  = Path(config.get("paths", "base"))
+SETUPDIR = BASEDIR / "scr/setup"
+#-------------------------------------------------------------------
+# PROCESS
+#-------------------------------------------------------------------
+if __name__ == "__main__":
+    try:
+        run("apt install -y --no-install-recommends usbguard")
+        run("usbguard generate-policy > /tmp/rules.conf")
+        run("install -m 0600 -o root -g root /tmp/rules.conf /etc/usbguard/rules.conf")
+        run("systemctl enable usbguard.service")
+        run("systemctl start usbguard.service")
+    except Exception as e:
+        reason = str(e)
+        outlog.logError(f"Failed to Harden Limits: {reason}")
+        raise e
