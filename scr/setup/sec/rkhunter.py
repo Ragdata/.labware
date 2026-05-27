a94dab4ea813756263aef1c7152d0ff5ab05f877
Ragdata <ragdata@users.noreply.github.com>
2026-05-28 07:35:59 +1000

    feat: add rkhunter

---
+++ scr/setup/sec/rkhunter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+====================================================================
+Package: labware
+====================================================================
+Author:			Ragdata
+Date:			12/05/2026
+License:		MIT License
+Repository:		https://github.com/Ragdata/.labware
+Copyright:		Copyright © 2026 Redeyed Technologies
+====================================================================
+"""
+import sys
+
+sys.path.append("../mod")
+
+from mod.filesys import *
+
+#-------------------------------------------------------------------
+# VARIABLES
+#-------------------------------------------------------------------
+BASEDIR  = Path(config.get("paths", "base"))
+SETUPDIR = BASEDIR / "scr/setup"
+#-------------------------------------------------------------------
+# PROCESS
+#-------------------------------------------------------------------
+if __name__ == "__main__":
+    try:
+        pkgs = ["rkhunter"]
+        installAPT(pkgs)
+        copyRepoFile(SETUPDIR, "/etc/default/rkhunter", True)
+        run("rkhunter --update")
+        run("rkhunter --propupd")
+    except Exception as e:
+        reason = str(e)
+        outlog.logError(f"Failed to install ACCT: {reason}")
+        raise e
