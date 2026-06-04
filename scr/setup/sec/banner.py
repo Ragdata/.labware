0ec4f86c8a675c3ba3eb7f4ccade6c56e23c6eb9
Ragdata <ragdata@users.noreply.github.com>
2026-06-04 11:43:42 +1000

    feat: add package banner

---
+++ scr/setup/sec/banner.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+====================================================================
+Package: labware
+====================================================================
+Author:			Ragdata
+Date:			12/05/2026
+License:		MIT License
+Repository:		https://github.com/Ragdata/.labware
+Copyright:		Copyright © 2026 Redeyed Technologies
+====================================================================
+"""
+import sys
+
+sys.path.append("../mod")
+
+from labware.logger import *
+
+#-------------------------------------------------------------------
+# PROCESS
+#-------------------------------------------------------------------
+def execute():
+    try:
+        # ----------------------------------------------------------
+        # SHOW LABWARE BANNER
+        # ----------------------------------------------------------
+        with open(BASEDIR / "sys/assets/ascii/labware.txt", "r") as f:
+            print(f.read())
+    except Exception as e:
+        logger.error(f"An error occurred: {e}", True)
+        raise
+
+# ===========================================================================
+# ENTRY POINT
+# ===========================================================================
+if __name__ == "__main__":
+    execute()
