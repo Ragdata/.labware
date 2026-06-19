a747b58cc807c3c226aeb276a0d52a1711cf85b4
Ragdata <Ragdata@users.noreply.gmail.com>
2026-06-19 14:21:39 +1000

    feat: update package

--- install.sh
+++ install.sh
@@ -93,10 +93,10 @@ echo
 # shellcheck disable=SC2181
 dpkg -l | grep python3-pip >/dev/null 2>&1 && dpkg -P python3-pip
 dpkg -l | grep python3-rich >/dev/null 2>&1 && dpkg -P python3-rich
-which python3.14 > /dev/null 2>&1; [[ "$?" -ne 0 ]] && apt install -y python3.14-full python3.14-venv
-export TMPDIR=/var/tmp
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py | python3.14
-apt install -y curl wget git gnupg2 net-tools rsync dnsutils iputils-ping procps python3-pip python3-dev build-essential
+which python3.14 > /dev/null 2>&1; [[ "$?" -ne 0 ]] && apt install -y python3.14-full python3.14-venv python3-pip python3-dev
+# export TMPDIR=/var/tmp
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py | python3.14
+apt install -y curl wget git gnupg2 net-tools rsync dnsutils iputils-ping procps build-essential
 
 # echo -e "${YELLOW}Installing UV ...${NC}"
 # curl -LsSf https://astral.sh/uv/install.sh | sh
