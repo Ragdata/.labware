43783f44acd6e37758cec03bc5c399cf0d2f06af
Ragdata <Ragdata@users.noreply.gmail.com>
2026-06-24 14:56:16 +1000

    fix: fix password

--- scr/lab/genPasswordList.py
+++ scr/lab/genPasswordList.py
@@ -28,19 +28,20 @@ BASEDIR = Path(config.get("paths", "base"))
 LABDIR = Path(config.get("paths", "lab"))
 DEFAULT_FILE = LABDIR / "cfg" / "pwd-sources.cfg"
 DEFAULT_DEST = Path("/usr/share/dict/passwords")
+TMPDIR = tempfile.mkdtemp()
 #-------------------------------------------------------------------
 # PROCESS
 #-------------------------------------------------------------------
 def execute(configfile: Path = DEFAULT_FILE, destfile: Path = DEFAULT_DEST):
     try:
 
+        logger.info(f"Temporary directory: {TMPDIR}")
+
         if not configfile.exists():
             raise FileNotFoundError(f"File not found: {configfile}")
 
-        with tempfile.TemporaryDirectory() as tmpdir:
-            tmpdir = Path(tmpdir)
-
-        logger.info(f"Temporary directory: {tmpdir}", True)
+        # with tempfile.TemporaryDirectory() as tmp:
+        #     tmpdir = Path(tmp)
 
         files = []
 
@@ -51,7 +52,7 @@ def execute(configfile: Path = DEFAULT_FILE, destfile: Path = DEFAULT_DEST):
                     logger.default(f"Processing URL: {url}", True)
                     purl = Path(urlparse(url).path)
                     filename = purl.name
-                    filetemp = Path(tmpdir) / filename
+                    filetemp = Path(TMPDIR) / filename
                     request.urlretrieve(url, str(filetemp))
                     logger.success(f"Downloaded to {filetemp}", True)
                     files.append(str(filetemp))
@@ -66,6 +67,8 @@ def execute(configfile: Path = DEFAULT_FILE, destfile: Path = DEFAULT_DEST):
     except Exception as e:
         logger.error(f"Script encountered an error: {e}", True)
         raise
+    finally:
+        shutil.rmtree(TMPDIR)
 
 
 # ===========================================================================
