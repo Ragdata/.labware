4e65d8a6dc8e35ec0c95dd62be4c9436c1b6733d
Ragdata <Ragdata@users.noreply.gmail.com>
2026-06-25 12:00:31 +1000

    feat: update compilers

--- scr/setup/sec/compilers.py
+++ scr/setup/sec/compilers.py
@@ -50,7 +50,7 @@ def execute():
                     chmod(comp, 0o750)
         ascomp = run("command -v as", capture=True).stdout.strip()
         if Path(ascomp).is_file() and os.access(ascomp, os.X_OK):
-            chmod(run("readlink -eq $(command -v as)", capture=True).stdout.strip(), 0o750)
+            chmod(Path(run("readlink -eq $(command -v as)", capture=True).stdout.strip()), 0o750)
         line()
         getData(f"[{yellow}]MODULE COMPLETE :: Press [ENTER] to continue ...[/{yellow}] ")
     except Exception as e:
